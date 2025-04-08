b4eef66d1273374fa76d28fe7d51fd479ed67a19
newermain <80409649+Newermain@users.noreply.github.com>
2025-04-08 17:47:51 +0400

    Update main.py

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Конфигурация
-BOT_TOKEN = "8125237649:AAHiHUVctjsSamLG7V_AH5TBbkofDLe8p3w"
-ADMIN_CHAT_ID = -1002595180902
-ADMIN_ID = 814124459
-DATABASE_NAME = "requests.db"
+BOT_TOKEN = "token_bot"
+ADMIN_CHAT_ID = chat_id
+ADMIN_ID = id_admin
+DATABASE_NAME = "name.db"
 
 # Инициализация бота
 storage = MemoryStorage()
